74df12ab39bd6bfad23e037186545e18e746df47
Praveen Kumar <praveenscience@gmail.com>
2022-03-20 08:20:26 +0000

    Added skeleton for listing and adding blood donors.

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,18 @@ class App extends Component {
         <Header dark={true} className="Header justify-content-center">
           CBIT Blood Donor App
         </Header>
+        <main>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-12 col-md-8">
+                <h2>List of Blood Donors</h2>
+              </div>
+              <div className="col-12 col-md-4">
+                <h2>Add yourself as Blood Donor</h2>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
