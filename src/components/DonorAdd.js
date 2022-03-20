02e8804deabf74a79e0e826ee937f9ea3e10f5f0
Praveen Kumar <praveenscience@gmail.com>
2022-03-20 08:32:35 +0000

    Created the form contents inside Donor Form.

--- src/components/DonorAdd.js
+++ src/components/DonorAdd.js
@@ -1,9 +1,36 @@
-import React from "react";
+import FormGroup from "./Bootstrap/FormGroup";
 
 const DonorAdd = () => {
   return (
     <div className="DonorAdd col-12 col-md-4">
       <h2>Add yourself as Blood Donor</h2>
+      <form>
+        {["Name", "Contact Number"].map(fg => (
+          <FormGroup
+            key={fg}
+            Label={fg}
+            Id={fg.replace(" ", "")}
+            Placeholder={`Please enter your ${fg}.`}
+          />
+        ))}
+        <div className="form-group">
+          <label htmlFor="BloodGroup">Blood Group</label>
+          <select name="BloodGroup" id="BloodGroup" className="custom-select">
+            <option value="" disabled="disabled">
+              Select your Blood Group
+            </option>
+            <option value="A+">A+</option>
+            <option value="B+">B+</option>
+            <option value="AB+">AB+</option>
+            <option value="O+">O+</option>
+            <option value="A-">A-</option>
+            <option value="B-">B-</option>
+            <option value="AB-">AB-</option>
+            <option value="O-">O-</option>
+          </select>
+        </div>
+        <input type="submit" className="btn btn-primary" value="Add Me!" />
+      </form>
     </div>
   );
 };
