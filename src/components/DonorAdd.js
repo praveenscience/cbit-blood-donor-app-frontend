3f5768df41755d4ac29cfc32a02e9c1cae321873
Praveen Kumar <praveenscience@gmail.com>
2022-03-20 08:40:16 +0000

    Use Controlled Components to get values.

--- src/components/DonorAdd.js
+++ src/components/DonorAdd.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import FormGroup from "./Bootstrap/FormGroup";
 
 const DonorAdd = () => {
+  const [Name, setName] = useState("");
+  const [ContactNumber, setContactNumber] = useState("");
+  const [BloodGroup, setBloodGroup] = useState("");
+
+  const Fields = {
+    Name: [Name, setName],
+    ContactNumber: [ContactNumber, setContactNumber]
+  };
+
   return (
     <div className="DonorAdd col-12 col-md-4">
       <h2>Add yourself as Blood Donor</h2>
@@ -10,12 +20,20 @@ const DonorAdd = () => {
             key={fg}
             Label={fg}
             Id={fg.replace(" ", "")}
+            Value={Fields[fg.replace(" ", "")][0]}
+            onChange={e => Fields[fg.replace(" ", "")][1](e.target.value)}
             Placeholder={`Please enter your ${fg}.`}
           />
         ))}
         <div className="form-group">
           <label htmlFor="BloodGroup">Blood Group</label>
-          <select name="BloodGroup" id="BloodGroup" className="custom-select">
+          <select
+            name="BloodGroup"
+            id="BloodGroup"
+            className="custom-select"
+            value={BloodGroup}
+            onChange={e => setBloodGroup(e.target.value)}
+          >
             <option value="" disabled="disabled">
               Select your Blood Group
             </option>
